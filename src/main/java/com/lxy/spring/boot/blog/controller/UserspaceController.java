fbcdbb3cc2a67e1e53097f0662a5c416eac40bd3
LiXinYang <lxylearning@outlook.com>
2022-09-14 16:42:40 +0800

    实现了完整的页面跳转，部分功能因时间问题待完善

--- src/main/java/com/lxy/spring/boot/blog/controller/UserspaceController.java
+++ src/main/java/com/lxy/spring/boot/blog/controller/UserspaceController.java
@@ -1,109 +1,28 @@
 package com.lxy.spring.boot.blog.controller;
 
-import com.lxy.spring.boot.domain.User;
-import com.lxy.spring.boot.service.UserService;
-import com.lxy.spring.boot.vo.Response;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.Optional;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * 用户主页空间控制器.
+ * User space controller.
  */
 @Controller
 @RequestMapping("/u")
 public class UserspaceController {
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private UserService userService;
 
     @GetMapping("/{username}")
     public String userSpace(@PathVariable("username") String username) {
         System.out.println("username" + username);
-        return "u";
+        return "/userspace/u";
     }
-
-    @GetMapping("/{username}/profile")
-    @PreAuthorize("authentication.name.equals(#username)")
-    public ModelAndView profile(@PathVariable("username") String username, Model model) {
-        User  user = (User)userDetailsService.loadUserByUsername(username);
-        model.addAttribute("user", user);
-        return new ModelAndView("/userspace/profile", "userModel", model);
-    }
-
-    /**
-     * 保存个人设置
-     * @param user
-     * @return
-     */
-    @PostMapping("/{username}/profile")
-    @PreAuthorize("authentication.name.equals(#username)")
-    public String saveProfile(@PathVariable("username") String username,User user) {
-        User originalUser = userService.getUserById(user.getId());
-        originalUser.setEmail(user.getEmail());
-        originalUser.setName(user.getName());
-
-        // 判断密码是否做了变更
-        String rawPassword = originalUser.getPassword();
-        PasswordEncoder  encoder = new BCryptPasswordEncoder();
-        String encodePasswd = encoder.encode(user.getPassword());
-        boolean isMatch = encoder.matches(rawPassword, encodePasswd);
-        if (!isMatch) {
-            originalUser.setEncodePassword(user.getPassword());
-        }
-
-        userService.saveUser(originalUser);
-        return "redirect:/u/" + username + "/profile";
-    }
-
-    /**
-     * 获取编辑头像的界面
-     * @param username
-     * @param model
-     * @return
-     */
-    @GetMapping("/{username}/avatar")
-    @PreAuthorize("authentication.name.equals(#username)")
-    public ModelAndView avatar(@PathVariable("username") String username, Model model) {
-        User  user = (User)userDetailsService.loadUserByUsername(username);
-        model.addAttribute("user", user);
-        return new ModelAndView("/userspace/avatar", "userModel", model);
-    }
-
-
-    /**
-     * 获取编辑头像的界面
-     * @param username
-     * @return
-     */
-    @PostMapping("/{username}/avatar")
-    @PreAuthorize("authentication.name.equals(#username)")
-    public ResponseEntity<Response> saveAvatar(@PathVariable("username") String username, User user) {
-        String avatarUrl = user.getAvatar();
-
-        User originalUser = userService.getUserById(user.getId());
-        originalUser.setAvatar(avatarUrl);
-        userService.saveUser(originalUser);
-
-        return ResponseEntity.ok().body(new Response(true, "处理成功", avatarUrl));
-    }
-
 
     @GetMapping("/{username}/blogs")
     public String listBlogsByOrder(@PathVariable("username") String username,
@@ -115,13 +34,13 @@ public class UserspaceController {
 
             System.out.print("category:" +category );
             System.out.print("selflink:" + "redirect:/u/"+ username +"/blogs?category="+category);
-            return "/u";
+            return "/userspace/u";
 
         } else if (keyword != null && keyword.isEmpty() == false) {
 
             System.out.print("keyword:" +keyword );
             System.out.print("selflink:" + "redirect:/u/"+ username +"/blogs?keyword="+keyword);
-            return "/u";
+            return "/userspace/u";
         }
 
         System.out.print("order:" +order);
@@ -133,13 +52,13 @@ public class UserspaceController {
     public String listBlogsByOrder(@PathVariable("id") Long id) {
 
         System.out.print("blogId:" + id);
-        return "/blog";
+        return "/userspace/blog";
     }
 
 
     @GetMapping("/{username}/blogs/edit")
     public String editBlog() {
 
-        return "/blogedit";
+        return "/userspace/blogedit";
     }
 }
